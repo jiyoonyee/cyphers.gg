bc7349439b34bacdce568f5cedd31a72504b6b8b
jiyoonyee <jyn1377@naver.com>
2023-09-01 18:54:33 +0900

    API

--- js/main_js.js
+++ js/main_js.js
@@ -1,3 +1,26 @@
-fetch('https://api.neople.co.kr/cy/players?nickname=<nickname>&wordType=<wordType>&apikey=<apikey>')
- 	.then(res => res.json())
-    .then(data => console.log(data));
+const inputNickname = document.querySelector(".input_nickname");
+const searchButton = document.querySelector(".search_button")
+let playerNickname = "";
+const wordType = "full"
+
+// 유저아이디 받아오기
+inputNickname.addEventListener("change",function(){
+    playerNickname = document.querySelector(".input_nickname").value;
+    console.log(playerNickname)
+})
+
+async function searchPlayer(nickname,wordType){
+     try{
+     const response = await fetch("https://api.neople.co.kr/cy/players?nickname=<nickname>&wordType=<wordType>&apikey=BBm56lzP9IxtGbM0tPRw0y0prd0ATmAR");
+     const searchData = await response.json();
+     console.log('search result',searchData);
+     } catch (error){
+         console.error('An error occurred'.error);
+     }  
+ }
+ 
+ searchButton.addEventListener("click",function(){
+    searchPlayer(playerNickname,"full");
+ })
+ 
+
