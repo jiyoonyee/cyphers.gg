87ae74ec92d49ce5c8d1d01236d8850cf7af9bc5
jiyoonyee <jyn1377@naver.com>
2023-09-16 20:14:18 +0900

    match search

--- js/main_js.js
+++ js/main_js.js
@@ -1,8 +1,10 @@
+// main.html
+
 //api 변수
 const inputNickname = document.querySelector(".input_nickname");
 const searchButton = document.querySelector(".search_button");
-let playerNickname = "";
-const wordType = "full";
+let playerNickname = null;
+let playerId = null;
 
 // 랭킹 이벤트 변수
 const rankingMenu = document.querySelector(".ranking_menu")
@@ -11,31 +13,49 @@ const close = document.querySelector(".close")
 
 
 // 유저아이디 받아오기
-inputNickname.addEventListener("change", function () {
-  playerNickname = document.querySelector(".input_nickname").value;
-});
 
 async function searchPlayer(nickname, wordType) {
   try {
     const apiUrl = `https://api.neople.co.kr/cy/players?nickname=${nickname}&wordType=${wordType}&apikey=BBm56lzP9IxtGbM0tPRw0y0prd0ATmAR`;
     const response = await fetch(apiUrl);
-    const searchData = await response.json();
-    const playerId = (searchData.rows[0].playerId);
-    const represent = (searchData.rows[0].represent);
-    const grade = (searchData.rows[0].grade)
-    console.log(searchData.rows[0]);
-    
+    let searchData = await response.json();
+    if(!searchData.rows || searchData.rows.length === 0){
+      alert("존재하지 않는 닉네임 입니다.")
+    }
+    else if(searchData.rows[0].nickname === playerNickname){
+      playerId = searchData.rows[0].playerId;
+      localStorage.setItem("playerNickname",searchData.rows[0].nickname);
+      location.href = "infor.html";
+    }
   } catch (error) {
     console.error('An error occurred:', error);
   }
 }
 
-searchButton.addEventListener("click", function () {
-  searchPlayer(playerNickname, wordType);
+inputNickname.addEventListener("keydown", function(event){
+  if(event.keyCode === 13){
+    playerNickname = inputNickname.value;
+    
+    if(playerNickname == "" || inputNickname.value.length<2){
+      alert("닉네임을 다시 입력해주세요");
+      inputNickname.value = "";
+    }
+    else{
+      searchPlayer(playerNickname,"match")
+    }
+    
+  }
 });
-inputNickname.addEventListener("change", function () {
-  searchPlayer(playerNickname, wordType);
-  
+
+searchButton.addEventListener("click", function () {
+  playerNickname = inputNickname.value;
+  if(playerNickname == "" || inputNickname.value.length<2){
+    alert("닉네임을 다시 입력해주세요");
+    inputNickname.value = "";
+  }
+  else{
+    searchPlayer(playerNickname,"match")
+  }
 });
 
 //랭킹 이벤트
